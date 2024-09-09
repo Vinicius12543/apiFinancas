9d8307f559b2cd429e6484cc529df0454d811bc3
Vinicius12543 <vduartedossantos12@gmail.com>
2024-09-09 19:41:38 -0300

    first commit

---
+++ src/test/java/br/com/cottinformatica/ApiFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cottinformatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
